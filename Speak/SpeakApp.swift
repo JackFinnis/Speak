90976e981fd7e8df81f4f0be8353f87044615e75
Jack Finnis <jack.finnis@icloud.com>
2022-12-16 23:07:10 +0000

    Initial Commit

---
+++ Speak/SpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakApp.swift
+//  Speak
+//
+//  Created by Jack Finnis on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
